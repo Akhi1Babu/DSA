a2c70bd43cd68b1db3425f3a812ae77f5a919c8a
Akhil babu <akhilb732@gmail.com>
2025-12-16 13:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (37.84%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int one=0;
         int two=0;
         int n=nums.size();
