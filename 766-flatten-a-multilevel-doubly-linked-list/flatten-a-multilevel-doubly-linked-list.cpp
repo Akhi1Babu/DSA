c03f852befd77281dffb557b62aa5f64749a3240
Akhil babu <akhilb732@gmail.com>
2026-01-03 19:37:39 +0530

    Time: 3 ms (65.22%) | Memory: 12 MB (9.53%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+private:
+    void generating(Node * node,vector<int> &result){
+        result.push_back(node->val);
+
+        if(node->child){
+            generating(node->child,result);
+        }
+        if(node->next){
+            generating(node->next,result);
+        }
+        else{
+            return;
+        }
+    }
+
+public:
+    Node* flatten(Node* head) {
+        if(head==NULL){
+            return NULL;
+        }
+        vector<int> result;
+        generating(head,result);
+        Node* resulthead=new Node(1,nullptr,nullptr,nullptr);
+        Node* resulttemp=resulthead;
+        Node* resultprev=resulthead;
+        for(int i=0;i<result.size();i++){
+            Node* newnode=new Node(result[i],nullptr,nullptr,nullptr);
+            resultprev=resulttemp;
+            newnode->prev=resultprev;
+            resulttemp->next=newnode;
+            resulttemp=resulttemp->next;
+
+        }
+        Node* head2=resulthead->next;
+        head2->prev=nullptr;
+        return head2;
+        
+    }
+};
