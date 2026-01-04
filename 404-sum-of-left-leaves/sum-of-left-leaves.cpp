6de06ee012e8e3fa7edb02007c7237c38d312a80
Akhil babu <akhilb732@gmail.com>
2026-01-04 22:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (83.74%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -1,45 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private: 
-    void findSum(TreeNode * root,int &summation){
-      if (!root) return;
-
-        // check if left child exists and is a leaf
-        if (root->left &&
-            root->left->left == NULL &&
-            root->left->right == NULL) {
-            summation += root->left->val;
+private:
+    void findSum(TreeNode* root, int& summation) {
+        if(!root){
+            return;
         }
-
+        if (isLeaf(root)) {
+            summation = summation + root->val;
+            return;
+        }
         findSum(root->left, summation);
-        findSum(root->right, summation);
-    }
-    bool isLeaf(TreeNode* node){
-        if(node->left==NULL && node->right==NULL){
-            return true;
+        if (!isLeaf(root->right)) {
+            findSum(root->right, summation);
         }
-        else{
+    }
+    bool isLeaf(TreeNode* node) {
+        if (node==NULL ||node->left == NULL && node->right == NULL) {
+            return true;
+        } else {
             return false;
         }
     }
+
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        int summation=0;
-        if(isLeaf(root)){
+        int summation = 0;
+        if (isLeaf(root)) {
             return 0;
         }
-        findSum(root,summation);
+        findSum(root, summation);
         return summation;
-        
     }
 };
