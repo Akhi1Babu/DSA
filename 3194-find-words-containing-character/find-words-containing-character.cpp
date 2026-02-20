8d7ec0f448ff49388e6da5d5ee629aae207283fb
Akhil babu <akhilb732@gmail.com>
2026-02-20 22:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (81.11%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -4,9 +4,11 @@ public:
         int n=words.size();
         vector<int> result;
         for(int i=0;i<n;i++){
-            sort(words[i].begin(),words[i].end());
-            if(binary_search(words[i].begin(),words[i].end(),x)){
-                result.push_back(i);
+           
+            auto it=find(words[i].begin(),words[i].end(),x);
+               
+            if(it!=words[i].end()){
+                 result.push_back(i);
             }
         }
         return result;
