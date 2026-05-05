0925201f88cebeca5acdc0e85da9a82a02e22524
Akhil babu <akhilb732@gmail.com>
2026-05-05 17:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.18%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        bool sign = true;
-        if(dividend< 0 && divisor<0){
-            sign=true;
-        }
-        else if (dividend < 0 || divisor < 0) {
-            sign = false;
-        }
-        if (dividend == INT_MIN && divisor == -1) {
-            return INT_MAX;
-        }
+     
         if(dividend==INT_MIN && divisor==1){
             return INT_MIN;
         }
-      
-      long long a = llabs((long long) dividend);
-long long b = llabs((long long) divisor);
-
-        int ans = 0;
-        while (a>=b) {
-              long long maxpow = 0;
-
-            while (a >= b << maxpow) {
-                maxpow++;
-                if(a<b<<maxpow){
-                    maxpow--;
+        else if(dividend==INT_MAX && divisor==1){
+            return INT_MAX;
+        }
+        else if(dividend==INT_MIN && divisor==-1){
+            return INT_MAX;
+        }
+        int answer=0;
+        bool negative=false;
+        if(dividend<0 && divisor<0){
+            negative=false;
+            
+        }
+        else if(dividend<0 || divisor<0){
+            negative=true;
+           
+        }
+        long long divid = abs((long long)dividend);
+long long div = abs((long long)divisor);
+        while(div<=divid){
+            int maxsize=-1;
+            int i=0;
+            while(i<32){
+                if((div<<i)<=divid){
+                    maxsize=i;
+                    i++;
+                }
+                else{
                     break;
                 }
             }
             
-            ans +=( 1 << maxpow);
-            a = a - (b << maxpow);
+               answer+=(1<<maxsize);
+            divid-=(div<<maxsize);
+         
+
         }
-        if(sign==false  && ans==INT_MAX){
-            return -1*INT_MAX;
-        }
-        if(sign==false){
-            return (int)-1*ans;
+        if(negative==true){
+            return -1*answer;
         }
         else{
-            return (int)ans;
+            return answer;
         }
     }
 };
